2073b248332b33cb9d0f0383a87c905293adf6ec
muhammad.noman <muhammad.noman@bentley.com>
2021-02-02 23:15:16 +0500

    unsuscribe message

--- WpfApp3/L/ViewModels/ReceiverViewModel.cs
+++ WpfApp3/L/ViewModels/ReceiverViewModel.cs
@@ -16,16 +16,31 @@ namespace L.ViewModels
         public ObservableCollection<string> M
         {
             get { return m; }
-            set {SetProperty(ref m , value); }
+            set {SetProperty(ref m , value); ea.Subscribe(s); }
         }
+        private bool show=true;
 
+        public bool Show
+        {
+            get { return show; }
+            set { SetProperty(ref show , value); hs(show); }
+        }
+        public WpfApp3.Core.Evenets.Events ea;
         public ReceiverViewModel(IEventAggregator  e)
         {
-            e.GetEvent<Events>().Subscribe(s,ThreadOption.PublisherThread,false,p=>p.Contains("Nomi")
-                );
+            ea = e.GetEvent<Events>();//.Subscribe(s,ThreadOption.PublisherThread,false,p=>p.Contains("Nomi")
+                                      //);
+            hs(true);
         }
         public void s(string ms) {
             M.Add(ms);
+        }
+
+        public void hs(bool isshow) {
+            if (isshow)
+                ea.Subscribe(s);
+            else
+                ea.Unsubscribe(s);
         }
     }
 }
